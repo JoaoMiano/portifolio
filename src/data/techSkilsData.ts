34606a3bda8f38d4c26f4b95f4001d5f0cc4c67b
Joao Miano <joaomiano15@gmail.com>
2026-04-11 12:47:13 -0300

    ajusted diretory images

--- src/data/techSkilsData.ts
+++ src/data/techSkilsData.ts
@@ -6,26 +6,26 @@ export type SkilsTech = {
 
 export const technologies: SkilsTech[] = [
     //Frontend
-    { name: 'HTML', logo: '/html.svg', category: "frontend" },
-    { name: 'CSS', logo: '/css.svg', category: "frontend" },
-    { name: 'Javascript', logo: '/javascript.svg', category: "frontend" },
-    { name: 'Typescript', logo: '/typescript.svg', category: "frontend" },
-    { name: 'React', logo: '/react.svg', category: "frontend" },
-    { name: 'NextJs', logo: '/nextjs.svg', category: "frontend" },
-    { name: 'TailwindCSS', logo: '/tailwind.svg', category: "frontend" },
-    { name: 'Figma', logo: '/figma.svg', category: "frontend" },
+    { name: 'HTML', logo: '/stacks/html.svg', category: "frontend" },
+    { name: 'CSS', logo: '/stacks/css.svg', category: "frontend" },
+    { name: 'Javascript', logo: '/stacks/javascript.svg', category: "frontend" },
+    { name: 'Typescript', logo: '/stacks/typescript.svg', category: "frontend" },
+    { name: 'React', logo: '/stacks/react.svg', category: "frontend" },
+    { name: 'NextJs', logo: '/stacks/nextjs.svg', category: "frontend" },
+    { name: 'TailwindCSS', logo: '/stacks/tailwind.svg', category: "frontend" },
+    { name: 'Figma', logo: '/stacks/figma.svg', category: "frontend" },
 
     //Backend
-    { name: 'NodeJs', logo: '/nodejs.svg', category: "backend" },
-    { name: 'ExpressJs', logo: '/expressjs.svg', category: "backend" },
-    { name: 'Prisma', logo: '/prisma.svg', category: "backend" },
-    { name: 'PostgreeSQL', logo: '/postgresql.svg', category: "backend" },
+    { name: 'NodeJs', logo: '/stacks/nodejs.svg', category: "backend" },
+    { name: 'ExpressJs', logo: '/stacks/expressjs.svg', category: "backend" },
+    { name: 'Prisma', logo: '/stacks/prisma.svg', category: "backend" },
+    { name: 'PostgreeSQL', logo: '/stacks/postgresql.svg', category: "backend" },
 
     //Devops
-    { name: 'Git', logo: '/git.svg', category: "devops" },
-    { name: 'GitHub', logo: '/github.svg', category: "devops" },
-    { name: 'AWS', logo: '/aws.svg', category: "devops" },
-    { name: 'Vercel', logo: '/vercel.svg', category: "devops" },
+    { name: 'Git', logo: '/stacks/git.svg', category: "devops" },
+    { name: 'GitHub', logo: '/stacks/github.svg', category: "devops" },
+    { name: 'AWS', logo: '/stacks/aws.svg', category: "devops" },
+    { name: 'Vercel', logo: '/stacks/vercel.svg', category: "devops" },
 
 ]
 
@@ -36,9 +36,9 @@ export const categoryLabels = {
 }
 
 export const BackendSkils:SkilsTech[] = [
-    { name: 'NodeJs', logo: '/nodejs.svg', category: "backend" },
-    { name: 'ExpressJs', logo: '/expressjs.svg', category: "backend" },
-    { name: 'Prisma', logo: '/prisma.svg', category: "backend" },
-    { name: 'PostgreeSQL', logo: '/postgresql.svg', category: "backend" },
+    { name: 'NodeJs', logo: '/stacks/nodejs.svg', category: "backend" },
+    { name: 'ExpressJs', logo: '/stacks/expressjs.svg', category: "backend" },
+    { name: 'Prisma', logo: '/stacks/prisma.svg', category: "backend" },
+    { name: 'PostgreeSQL', logo: '/stacks/postgresql.svg', category: "backend" },
     
 ]
